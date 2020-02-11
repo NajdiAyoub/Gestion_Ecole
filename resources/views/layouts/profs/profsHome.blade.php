bb968780d764a2925cc0408d35526c9cc0eacdf1
Najdi Ayoub <najdi14.ayoub@gmail.com>
2020-02-11 14:35:02 +0100

    profs view modified

--- resources/views/layouts/profs/profsHome.blade.php
+++ resources/views/layouts/profs/profsHome.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<center>
+<div class="topnav">
+  <strong><em><a class="active" href="#home">Emplois</a></em></strong>
+   <strong><em><a href="#news">Bulletins et Notes</a></em></strong>
+    <strong><em><a href="#contact">Cours</a></em></strong>
+    <strong><em><a href="#about">Evenements</a></em></strong>
+  </div>
+</center>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
