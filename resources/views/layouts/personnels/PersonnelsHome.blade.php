02050543c4c245baa7c8ddf38378cd42639b393c
Najdi Ayoub <najdi14.ayoub@gmail.com>
2020-02-12 10:14:21 +0100

    Menu Personnels Modified

--- resources/views/layouts/personnels/PersonnelsHome.blade.php
+++ resources/views/layouts/personnels/PersonnelsHome.blade.php
@@ -13,72 +13,39 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
-             <mark><center><strong><em>Welcome Admin.<em></strong></center></mark>
+             <center><strong><mark><em>Welcome Administrator<em></mark></strong></center>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <center>
-<button class="accordion">Espaces Profs</button>
-<div class="panel">
-  <p>Put a SubMenu For TeacherSpace..</p>
-</div>
-
-<button class="accordion">Espaces Eleves</button>
-<div class="panel">
-  <p>Put a SubMenu For StudentSpace..</p>
-</div>
-
-<button class="accordion">Affectations</button>
-<div class="panel">
-  <p>Put a SubMenu For Delegate..</p>
-</div>
+  <ul id="menu-accordeon">
+    <li><a href="#">EspacesProfs</a>
+       <ul>
+          <li><a href="#">GestionAbsencesProfs</a></li>
+          <li><a href="#">GestionPaiementsProfs</a></li>
+          <li><a href="#">GestionEmploisProfs</a></li>
+          <li><a href="#">lien sous menu 1</a></li>
+       </ul>
+    </li>
+     <li><a href="#">EspacesEleves</a>
+       <ul>
+          <li><a href="#">Lien sous menu 2</a></li>
+          <li><a href="#">Lien sous menu 2</a></li>
+          <li><a href="#">Lien sous menu 2</a></li>
+          <li><a href="#">Lien sous menu 2</a></li>
+       </ul>
+    </li>
+    <li><a href="#">EspacesAffectations</a>
+       <ul>
+          <li><a href="#">Lien sous menu 3</a></li>
+          <li><a href="#">Lien sous menu 3</a></li>
+          <li><a href="#">Lien sous menu 3</a></li>
+          <li><a href="#">Lien sous menu 3</a></li>
+       </ul>
+    </li>
+ </ul>
 </center>
-<script>
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-</script>
-<style>
-    .panel {
-      padding: 0 18px;
-      background-color: white;
-      max-height: 0;
-      overflow: hidden;
-      transition: max-height 0.2s ease-out;
-    }
-    </style>
-    
-    <script>
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-    
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      });
-    }
-    </script>
 @endsection
